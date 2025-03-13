d22037b0ab48d9feb10c344d273f579c8504753a
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-03-13 16:57:17 +0530

    comment the superuser start class

--- src/main/java/com/mcode/llp/codegen/initializer/SuperUserInitializer.java
+++ src/main/java/com/mcode/llp/codegen/initializer/SuperUserInitializer.java
@@ -22,38 +22,38 @@ public class SuperUserInitializer {
     }
 
     public void initializeOpenSearch() {
-        try {
-            // Check if the schema exists
-            response = openSearchClient.sendRequest(schemaEndPoint,"GET", null);
-            if(response.statusCode() == 404){
-                String requestData = "{\"title\":\"users\",\"properties\":{\"username\":{\"type\":\"string\"},\"password\":{\"type\":\"string\"},\"role\":{\"type\":\"string\"},\"tenant\":{\"type\":\"string\"}},\"required\":[\"username\",\"password\",\"role\",\"tenant\"]}";
-                response=openSearchClient.sendRequest(schemaEndPoint, "POST", requestData);
-                if (response.statusCode() == 201) {
-                    logger.info("✅ User Schema created successfully.");
-                } else {
-                    logger.error(ERROR,response.body());
-                }
-            } else {
-                logger.info("✅ User Schema already exists. Skipping initialization.");
-            }
-            // Check if the index exists
-            response = openSearchClient.sendRequest("/" + USER_INDEX, "GET", null);
-            if (response.statusCode() == 404) {
-                String requestData= "{\"username\":\"superadmin\",\"password\":\"SuperSecurePassword\",\"role\":\"superuser\",\"tenant\":\"global\"}";
-                String endpoint = "/" + USER_INDEX + "/_doc/" + 1;
-               response=openSearchClient.sendRequest(endpoint, "POST", requestData);
-                if (response.statusCode() == 201) {
-                    logger.info("✅ superUser index created successfully.");
-                } else {
-                    logger.error(ERROR,response.body());
-                }
-            } else {
-                logger.info("✅ User index already exists. Skipping initialization.");
-            }
-
-        } catch (IOException | InterruptedException e) {
-            logger.error(ERROR, e.getMessage());
-            Thread.currentThread().interrupt();
-        }
+//        try {
+//            // Check if the schema exists
+//            response = openSearchClient.sendRequest(schemaEndPoint,"GET", null);
+//            if(response.statusCode() == 404){
+//                String requestData = "{\"title\":\"users\",\"properties\":{\"username\":{\"type\":\"string\"},\"password\":{\"type\":\"string\"},\"role\":{\"type\":\"string\"},\"tenant\":{\"type\":\"string\"}},\"required\":[\"username\",\"password\",\"role\",\"tenant\"]}";
+//                response=openSearchClient.sendRequest(schemaEndPoint, "POST", requestData);
+//                if (response.statusCode() == 201) {
+//                    logger.info("✅ User Schema created successfully.");
+//                } else {
+//                    logger.error(ERROR,response.body());
+//                }
+//            } else {
+//                logger.info("✅ User Schema already exists. Skipping initialization.");
+//            }
+//            // Check if the index exists
+//            response = openSearchClient.sendRequest("/" + USER_INDEX, "GET", null);
+//            if (response.statusCode() == 404) {
+//                String requestData= "{\"username\":\"superadmin\",\"password\":\"SuperSecurePassword\",\"role\":\"superuser\",\"tenant\":\"global\"}";
+//                String endpoint = "/" + USER_INDEX + "/_doc/" + 1;
+//               response=openSearchClient.sendRequest(endpoint, "POST", requestData);
+//                if (response.statusCode() == 201) {
+//                    logger.info("✅ superUser index created successfully.");
+//                } else {
+//                    logger.error(ERROR,response.body());
+//                }
+//            } else {
+//                logger.info("✅ User index already exists. Skipping initialization.");
+//            }
+//
+//        } catch (IOException | InterruptedException e) {
+//            logger.error(ERROR, e.getMessage());
+//            Thread.currentThread().interrupt();
+//        }
     }
 }
